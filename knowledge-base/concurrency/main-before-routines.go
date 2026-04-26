eb9cdff0a94aa899dff74afa0bf528edc2953778
KhaledSaiidi <khaled.saiidi@outlook.com>
2026-04-26 20:27:48 +0100

    feat: knowledge-base/concurrency

---
+++ knowledge-base/concurrency/main-before-routines.go
@@ -0,0 +1,74 @@
+package main
+
+// import (
+// 	"fmt"
+// 	"math/rand"
+// 	"time"
+// )
+
+// type Order struct {
+// 	ID     int
+// 	Status string
+// }
+
+// func generateOrders(count int) []*Order {
+// 	orders := make([]*Order, count)
+// 	for i := 0; i < count; i++ {
+// 		orders[i] = &Order{
+// 			ID:     i + 1,
+// 			Status: "Pending",
+// 		}
+// 	}
+// 	return orders
+// }
+
+// func processOrders(orders []*Order) {
+// 	for _, order := range orders {
+// 		time.Sleep(
+// 			time.Duration(rand.Intn(500)) *
+// 				time.Millisecond,
+// 		)
+// 		fmt.Printf("Processing order %d\n", order.ID)
+// 	}
+// }
+
+// func updateOrderStatuses(orders []*Order) {
+// 	for _, order := range orders {
+// 		time.Sleep(
+// 			time.Duration(rand.Intn(500)) *
+// 				time.Millisecond,
+// 		)
+// 		status := []string{
+// 			"Processing", "Shipped", "Delivered",
+// 		}[rand.Intn(3)]
+
+// 		order.Status = status
+// 		fmt.Printf("Updated order %d, Status: %s\n", order.ID, order.Status)
+// 	}
+// }
+
+// func reportOrdersStaus(orders []*Order) {
+// 	for i := 0; i < 5; i++ {
+// 		time.Sleep(1 * time.Second)
+// 		fmt.Println("\n--- Order Status Report ---")
+// 		for _, order := range orders {
+// 			fmt.Printf(
+// 				"Order %d: %s\n",
+// 				order.ID, order.Status,
+// 			)
+// 		}
+// 		fmt.Println("--------------------------------------")
+// 	}
+// }
+
+// func main() {
+// 	orders := generateOrders(20)
+
+// 	processOrders(orders)
+
+// 	updateOrderStatuses(orders)
+
+// 	reportOrdersStaus(orders)
+
+// 	fmt.Println("All operations completed. Exiting.")
+// }
---
+++ knowledge-base/concurrency/main.go
@@ -0,0 +1,139 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+type Order struct {
+	ID     int
+	Status string
+	mutex  sync.Mutex
+}
+
+var (
+	totalUpdates int
+	updateMutex  sync.Mutex
+)
+
+func generateOrders(count int) []*Order {
+	orders := make([]*Order, count)
+	for i := 0; i < count; i++ {
+		orders[i] = &Order{
+			ID:     i + 1,
+			Status: "Pending",
+		}
+	}
+	return orders
+}
+
+func processOrders(
+	orderChan <-chan *Order, // receive only channel,
+	processedOrderChan chan<- *Order, // send only channel,
+	wg *sync.WaitGroup,
+) {
+	defer func() {
+		wg.Done()
+		close(processedOrderChan)
+	}()
+	for order := range orderChan {
+		time.Sleep(
+			time.Duration(rand.Intn(500)) *
+				time.Millisecond,
+		)
+		order.Status = "Processed"
+		processedOrderChan <- order
+	}
+}
+
+func updateOrderStatuses(order *Order) {
+	order.mutex.Lock()
+	defer order.mutex.Unlock()
+	time.Sleep(
+		time.Duration(rand.Intn(500)) *
+			time.Millisecond,
+	)
+	status := []string{
+		"Processing", "Shipped", "Delivered",
+	}[rand.Intn(3)]
+
+	order.Status = status
+	fmt.Printf("Updated order %d, Status: %s\n", order.ID, order.Status)
+
+	updateMutex.Lock()
+	defer updateMutex.Unlock()
+	currentUpdates := totalUpdates
+	time.Sleep(5 * time.Millisecond)
+	totalUpdates = currentUpdates + 1
+}
+
+func reportOrdersStaus(orders []*Order) {
+	fmt.Println("\n--- Order Status Report ---")
+	for _, order := range orders {
+		fmt.Printf(
+			"Order %d: %s\n",
+			order.ID, order.Status,
+		)
+	}
+	fmt.Println("--------------------------------------")
+}
+
+func main() {
+	wg := sync.WaitGroup{}
+
+	// orderChan := make(chan *Order) 		-> Unbuffered Channel No capaciy and Synchronous
+	orderChan := make(chan *Order, 20) //   -> Buffered Channel has 20 capaciy and Asynchronous
+	processedOrderChan := make(chan *Order, 20)
+	var orders []*Order
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		defer close(orderChan)
+		for _, order := range generateOrders(20) {
+			orderChan <- order
+			orders = append(orders, order)
+		}
+
+		fmt.Println("Done with generating orders")
+	}()
+	wg.Add(1)
+	go processOrders(orderChan, processedOrderChan, &wg)
+
+	wg.Add(1)
+
+	go func() {
+		defer wg.Done()
+		for {
+			select {
+			case processedOrder, ok := <-processedOrderChan:
+				if !ok {
+					fmt.Println("Processing channel closed")
+					return
+				}
+				fmt.Printf("Processed order %d with status: %s\n", processedOrder.ID, processedOrder.Status)
+			case <-time.After(10 * time.Second):
+				fmt.Println("No processed orders received in the last second")
+				return
+			}
+		}
+	}()
+	wg.Add(3)
+	for range 3 {
+		go func() {
+			defer wg.Done()
+			for _, order := range orders {
+				updateOrderStatuses(order)
+			}
+		}()
+	}
+
+	wg.Wait()
+	reportOrdersStaus(orders)
+
+	fmt.Println("All operations completed. Exiting.")
+	fmt.Println(totalUpdates)
+
+}
